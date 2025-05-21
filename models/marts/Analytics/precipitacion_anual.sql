f4cd77a59ca21c02dd7378bdadbfda067a1ab10c
Jose Manuel Perez <drontastico@gmail.com>
2025-05-21 09:35:02 +0000

    miercoles

--- models/marts/Analytics/precipitacion_anual.sql
+++ models/marts/Analytics/precipitacion_anual.sql
@@ -5,12 +5,10 @@
 ) }}
 
 SELECT
-  d.anio           AS anio,
-  SUM(p.precipitacion)  AS precipitacion_anual
-FROM fact_meteo      f
--- Unimos con la dimensión de fecha para extraer Año
-JOIN dim_fecha       d ON f.fecha_id = d.fecha_id
--- Unimos con la dimensión de precipitación para obtener el valor
-JOIN dim_precipitacion p ON f.lluvia_id = p.lluvia_id
+  d.anio                      AS anio,
+  sum(p.precipitacion)        AS precipitacion_anual
+FROM {{ ref('fact_meteo') }}              f
+JOIN {{ ref('dim_fecha') }}       d ON f.fecha_id = d.fecha_id
+JOIN {{ ref('dim_precipitacion') }} p ON f.lluvia_id = p.lluvia_id
 GROUP BY 1
-ORDER BY 1;
+ORDER BY 1
